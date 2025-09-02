2bcb434cbe6574f06a340fd8945b067385ef937d
Evgeniy <evgnec@gmail.com>
2025-09-03 00:32:10 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
+import React from 'react'
+import TodDoList from './ToDo/TodDoList'
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <TodDoList/>
     </div>
-  );
-};
+  )
+}
+
+export default App
+
---
+++ src/components/ToDo/ToDo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToDo = () => {
+  return (
+    <div>
+      1 ToDo
+    </div>
+  )
+}
+
+export default ToDo
